7149ed42af21c060982d7ebc6ec54ee2301f5d2a
guptavarun619 <guptavarun619@gmail.com>
2022-12-31 15:54:28 +0530

    [fix] removed the test mail being sent every time the server start

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const { sendBasicEmail } = require("./services/email-service");
+// const { sendBasicEmail } = require("./services/email-service");
 
 const { PORT } = require("./config/serverConfig");
 
@@ -14,12 +14,12 @@ const initializeServer = () => {
   app.listen(PORT, async () => {
     console.log(`Notification server started at PORT : ${PORT}`);
 
-    sendBasicEmail(
-      "support@admin.com",
-      "nw6k5@emergentvillage.org",
-      "TEST EMAIL",
-      "This is a test email from notification service"
-    );
+    // sendBasicEmail(
+    //   "support@admin.com",
+    //   "nw6k5@emergentvillage.org",
+    //   "TEST EMAIL",
+    //   "This is a test email from notification service"
+    // );
   });
 };
 
